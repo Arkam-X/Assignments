6affc2e9dd255231e54c347356cabe3169f4c28a
Arkam-X <charkam786@gmail.com>
2025-09-27 13:57:14 +0530

    Initial commit - added assignment code

---
+++ task1.py
@@ -0,0 +1,31 @@
+from sqlalchemy import create_engine, text
+from urllib.parse import quote_plus
+import pandas as pd
+import duckdb as db
+
+# 1. Connecting to MSSQL
+connection_str = (
+    "DRIVER={ODBC Driver 17 for SQL Server};"
+    "SERVER=XPERIA\SQLEXPRESS;"
+    "DATABASE=TravelAgencyDB;"
+    "UID=sa;"
+    "Trusted_Connection=yes;"
+    "TrustServerCertificate=yes;"
+)
+conn_url = "mssql+pyodbc:///?odbc_connect=" + quote_plus(connection_str)
+engine = create_engine(conn_url)
+
+# 2. Extract data from MSSQL and store in df.
+with engine.connect() as conn:
+    df = pd.read_sql_query(text("SELECT * FROM TourBookings"), conn)
+
+# 3. Convert the data to parquet.
+df.to_parquet("tour_bookings.parquet", engine="pyarrow", index=False)
+
+# 4. Load the data into DUCK.
+con = db.connect("travelduck.duckdb")
+con.execute("CREATE TABLE tour_bookings AS SELECT * FROM read_parquet('tour_bookings.parquet')")
+
+# 5. Run queries to see the implemention.
+print(con.execute("SELECT COUNT(*) FROM tour_bookings").fetchall())
+print(con.execute("SELECT * FROM tour_bookings LIMIT 5").fetchdf())
